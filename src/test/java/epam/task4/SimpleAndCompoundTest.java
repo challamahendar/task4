b92f462d091face26f0558dd1db18b81015018ef
challamahendar <challa.mahendar9@gmail.com>
2020-07-21 18:19:50 +0530

    Initial commit

---
+++ src/test/java/epam/task4/SimpleAndCompoundTest.java
@@ -0,0 +1,20 @@
+package epam.task4;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class SimpleAndCompoundTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
